d39dd7751415ab963c0bd522b7ae6c0b249bee4b
Unity Game Development & Tutorial Code <83016119+UnityFreeGames@users.noreply.github.com>
2023-02-28 23:50:11 +0330

    Add files via upload

---
+++ Assets/Scripts/LaserReflector.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserReflector : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    float rotationSpeed = 0.2f;
+
+    Vector3 position;
+    Vector3 direction;
+    LineRenderer lr;
+    public bool isOpen;
+
+    GameObject tempReflector;
+    void Start()
+    {
+        isOpen = false;
+        lr = gameObject.GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        if (isOpen)
+        {
+            lr.positionCount = 2;
+            lr.SetPosition(0, position);
+            RaycastHit hit;
+            if (Physics.Raycast(position, direction, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.CompareTag("Reflector"))
+                {
+                    tempReflector = hit.collider.gameObject;
+                    Vector3 temp = Vector3.Reflect(direction, hit.normal);
+                    hit.collider.gameObject.GetComponent<LaserReflector>().OpenRay(hit.point, temp);
+                }
+                lr.SetPosition(1, hit.point);
+            }
+            else
+            {
+                if (tempReflector)
+                {
+                    tempReflector.GetComponent<LaserReflector>().CloseRay();
+                    tempReflector=null;
+                }
+                lr.SetPosition(1,direction*100);
+            }
+        }
+        else
+        {
+            if (tempReflector)
+            {
+                tempReflector.GetComponent<LaserReflector>().CloseRay();
+                tempReflector=null;
+            }
+        }
+        
+    }
+    public void OpenRay(Vector3 pos,Vector3 dir)
+    {
+        isOpen = true;
+        position = pos;
+        direction = dir;
+    }
+    public void CloseRay()
+    {
+        isOpen = false;
+        lr.positionCount = 0;
+    }
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);    
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    
+    }
+ 
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);  
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+
+        //rotate cube
+        /*float XaxisRotation = Input.GetAxis("Mouse X")*rotationSpeed;
+		float YaxisRotation = Input.GetAxis("Mouse Y")*rotationSpeed;
+		transform.RotateAround(Vector3.down, XaxisRotation);
+		transform.RotateAround(Vector3.right, YaxisRotation);*/
+    
+    }
+}
